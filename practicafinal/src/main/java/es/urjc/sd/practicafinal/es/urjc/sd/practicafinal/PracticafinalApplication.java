942bddc9e263991c9b6e6a4b931bed2969b26c15
Campaa02 <pca6@hotmail.es>
2023-03-27 11:00:16 +0200

    w

--- practicafinal/src/main/java/es/urjc/sd/practicafinal/es/urjc/sd/practicafinal/PracticafinalApplication.java
+++ practicafinal/src/main/java/es/urjc/sd/practicafinal/es/urjc/sd/practicafinal/PracticafinalApplication.java
@@ -3,6 +3,8 @@ package es.urjc.sd.practicafinal;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+
 @SpringBootApplication
 public class PracticafinalApplication {
 
